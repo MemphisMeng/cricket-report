a4139ff88ecad85e75280d5de573f45c36b9963a
Anzhe Meng <meng@prepbaseballreport.com>
2023-09-02 12:58:32 -0400

    removed infra

--- data-extraction/app.py
+++ data-extraction/app.py
@@ -1,28 +1,88 @@
 import os
-from infra.lambda_function_stack import LambaFunctionStack
-from infra.iam_stack import IAMStack
-from infra.rds_stack import RdsStack
-from infra.rule_stack import RuleStack
-from aws_cdk import App, Tags
-
-# application initiation
-app = App()
-enviroment = app.node.try_get_context("environment")
-
-account = os.environ["CDK_DEFAULT_ACCOUNT"]
-region = os.environ["CDK_DEFAULT_REGION"]
-
-extraction_function = LambaFunctionStack(
-    app,
-    env=enviroment,
-    function="downloader",
-    code_directory="code/"
+from constructs import Construct
+from aws_cdk.aws_iam import Role, ServicePrincipal, PolicyDocument, PolicyStatement, Effect, ManagedPolicy
+from aws_cdk.aws_rds import (
+    Credentials,
+    DatabaseCluster,
+    DatabaseClusterEngine,
+    AuroraPostgresEngineVersion,
+    InstanceProps,
 )
-rds_cluster = RdsStack(
-    app,
-    environment=enviroment,
-    stack_name="CricketDB",
-    cluster_name="cricket"
+from aws_cdk.aws_ec2 import (
+    Vpc,
+    InstanceClass,
+    InstanceType,
+    InstanceSize,
+    SecurityGroup,
+    Port,
+    Peer,
 )
+from aws_cdk.aws_lambda import Function, Runtime, Code
+from aws_cdk import Stack
 
-app.synth()
+from aws_cdk import App
+
+class DataExtractionStack(Stack):
+    def __init__(
+        self, scope: Construct, construct_id: str, environment: str, code_directory:str, stack_name:str, cluster_name:str, **kwargs
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        self.role = Role(self, "ExecutionRole",
+                role_name=f"{enviroment}-execution-role",
+                description=f"The IAM Role assumed to execute the part that downloads data from the website to the database in the {env} environment",
+            managed_policies=[
+                'arn:aws:iam::aws:policy/AWSLambdaExecute',
+                'arn:aws:iam::aws:policy/AmazonSQSFullAccess',
+                'arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess'
+            ], inline_policies={
+                'misc-policies': PolicyDocument(
+                    statements=[
+                        PolicyStatement(
+                            actions=['events:PutEvents'],
+                            effect=Effect.ALLOW,
+                            resources=['*']
+                        ),
+                        PolicyStatement(
+                            actions=['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+                            effect=Effect.ALLOW,
+                            resources=['arn:aws:logs:*:*:*']
+                        )
+                    ]
+                )
+            })
+
+        self.downloader_function = Function(
+            self, f"{environment}-Downloader", 
+            runtime=Runtime.PYTHON_3_9,
+            handler="lambda_function.lambda_handler",
+            function_name=f"{enviroment}-downloader",
+            code=Code.from_asset(code_directory),
+            role=self.role
+            )
+        
+        self.rds = DatabaseCluster(
+            self,
+            f"{environment}-{stack_name}",
+            engine=DatabaseClusterEngine.aurora_postgres(
+                version=AuroraPostgresEngineVersion.VER_14_4
+            ),
+            # credentials=Credentials.from_secret(rds_secret),
+            cluster_identifier=f"{environment}-{cluster_name}",
+            # default_database_name="athlete_performance_metrics",
+            port=5432,
+            instance_props=InstanceProps(
+                instance_type=InstanceType.of(InstanceClass.T3, InstanceSize.MEDIUM),
+                # security_groups=[security_group],
+                # vpc=vpc,
+            ),
+        )
+
+if __name__ == '__main__':
+    # application initiation
+    app = App()
+    enviroment = app.node.try_get_context("environment")
+
+    account = os.environ["CDK_DEFAULT_ACCOUNT"]
+    region = os.environ["CDK_DEFAULT_REGION"]
+
+    app.synth()
