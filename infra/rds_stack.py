c70cdd7747399ebc7755e238e5fc75e4b3d730fb
Anzhe Meng <meng@prepbaseballreport.com>
2023-09-02 11:03:23 -0400

    Update rds_stack.py

--- infra/rds_stack.py
+++ infra/rds_stack.py
@@ -21,18 +21,18 @@ import json
 
 class RdsStack(Stack):
     def __init__(
-        self, scope: Construct, construct_id: str, environment: str, **kwargs
+        self, scope: Construct, construct_id: str, environment: str, stack_name:str, cluster_name:str, **kwargs
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
         self.cluster = DatabaseCluster(
             self,
-            "AthletePerformanceMetricsDB",
+            stack_name,
             engine=DatabaseClusterEngine.aurora_postgres(
                 version=AuroraPostgresEngineVersion.VER_14_4
             ),
             # credentials=Credentials.from_secret(rds_secret),
-            cluster_identifier=f"athlete-performance-metrics-cluster-{environment}",
-            default_database_name="athlete_performance_metrics",
+            cluster_identifier=cluster_name,
+            # default_database_name="athlete_performance_metrics",
             port=5432,
             instance_props=InstanceProps(
                 instance_type=InstanceType.of(InstanceClass.T3, InstanceSize.MEDIUM),
