68eba4941f4c8f3da2c14493ddd900d1951dac4b
Anzhe Meng <meng@prepbaseballreport.com>
2023-09-02 22:35:31 -0400

    created infra

---
+++ infra/infra/infra_stack.py
@@ -0,0 +1,22 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+from aws_cdk.aws_ec2 import Vpc, IpAddresses
+
+class InfraStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, environemnt:str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "InfraQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
+        self.vpc = Vpc(self, f"{environemnt}-Vpc",
+            ip_addresses=IpAddresses.cidr("10.0.0.0/16"))
