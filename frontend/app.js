d7650ac5b24fcd5bc306e95dcfdf284432afd7d0
Anzhe Meng <meng@prepbaseballreport.com>
2023-10-10 00:02:27 -0400

    displayed games played per team

--- frontend/app.js
+++ frontend/app.js
@@ -2,8 +2,10 @@
 const sqlite3 = require('sqlite3').verbose();
 var express = require ('express');
 var app = express ();
+app.use(express.static('public'));
+// app.engine('ejs', engine());
 app.set('view engine', 'ejs');
-app.set('views', '.views');
+app.set('views', './views');
 
 // open the database connection
 let db = new sqlite3.Database('./../zelus.db', (err) => {
@@ -21,27 +23,31 @@ app.get('/players', (req, res) => {
     //   res.sendFile('/index.html');
       res.render('index', { rows: rows });
     });
-
-    db.close((err) => {
-      if (err) {
-        console.error(err.message);
-      }
-    });
   });
 
 app.get('/vis', (req, res) => {
-  let sql = ``;
+  // games played per country
+  let sql = `WITH teams AS (
+      SELECT 
+      json_extract(teams, '$[0]') AS team1, 
+      json_extract(teams, '$[1]') AS team2
+      FROM match_results),
+      games AS(
+        SELECT team1 AS team, COUNT(*) AS games
+        FROM teams
+        GROUP BY 1
+        UNION ALL
+        SELECT team2 AS team, COUNT(*) AS games
+        FROM teams
+        GROUP BY 1)
+      SELECT team, SUM(games) AS games
+      FROM games
+      GROUP BY 1`;
   db.all(sql, (err, rows) => {
     if (err) {
       throw err;
     }
-    res.send(rows);
-  });
-
-  db.close((err) => {
-    if (err) {
-      console.error(err.message);
-    }
+    res.json(rows);
   });
 });
 
---
+++ frontend/public/static/js/bubble.js
@@ -0,0 +1,83 @@
+import * as d3 from "d3";
+import * as axios from "axios";
+
+const dataSet = async function getData() {
+    return await axios.get('/vis');
+}
+
+async function drawChart() {
+    const teams  = await dataSet();
+    const data = teams.map((d) => (
+        {
+            ...d,
+            team: d.team,
+            games: d.games
+        }
+    ))
+    .filter(d => d.team)
+    .sort((a, b) => d3.descending(a.games, b.games));
+
+    // construct the radius scale
+    const radius = d3.scaleSqrt([0, d3.max(data, d => d.population)], [0, 40]);
+    
+    // construct the path generator
+    const path = d3.geoPath();
+
+    // Select the SVG container. Its dimensions correspond to the bounding-box
+    const svg = d3.select("svg")
+        .attr("width", 975)
+        .attr("height", 610)
+        .attr("viewBox", [0, 0, 975, 610])
+        .attr("style", "width: 100%; height: auto; height: intrinsic;");
+
+    // Create the cartographic background layers.
+    svg.append("path")
+        .datum(topojson.feature(world, world.objects.land))
+        .attr("fill", "#ddd")
+        .attr("d", path);
+
+    svg.append("path")
+        .datum(topojson.mesh(world, world.objects.countries, (a, b) => a !== b))
+        .attr("fill", "none")
+        .attr("stroke", "white")
+        .attr("stroke-linejoin", "round")
+        .attr("d", path);
+
+    // Create the legend.
+    const legend = svg.append("g")
+        .attr("fill", "#777")
+        .attr("transform", "translate(915,608)")
+        .attr("text-anchor", "middle")
+        .style("font", "10px sans-serif")
+        .selectAll()
+        .data(radius.ticks(4).slice(1))
+        .join("g");
+
+    legend.append("circle")
+        .attr("fill", "none")
+        .attr("stroke", "#ccc")
+        .attr("cy", d => -radius(d))
+        .attr("r", radius);
+
+    legend.append("text")
+        .attr("y", d => -2 * radius(d))
+        .attr("dy", "1.3em")
+        .text(radius.tickFormat(4, "s"));
+
+    // Add a circle for each county, with a title (tooltip).
+    const format = d3.format(",.0f");
+    svg.append("g")
+        .attr("fill", "brown")
+        .attr("fill-opacity", 0.5)
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 0.5)
+        .selectAll()
+        .data(data)
+        .join("circle")
+        .attr("transform", d => `translate(${centroid(d.team)})`)
+        .attr("r", d => radius(d.games))
+        .append("title")
+        .text(d => `${d.team.properties.name}: ${format(d.games)}`);
+
+}
+drawChart();
